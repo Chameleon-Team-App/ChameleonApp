c76f76a28ba027b95a3da21e2cf5bcad9abfc030
ChristopherArias-Hash <christopher.arias855@myci.csuci.edu>
2024-10-18 23:52:08 -0700

    Photo gallery can now access camera. Camera can now go back to gallery

--- app/src/main/java/com/example/maincameleon/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/example/maincameleon/ui/camera/CameraFragment.kt
@@ -2,107 +2,129 @@ package com.example.maincameleon.ui.camera
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
-import androidx.camera.core.*
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.maincameleon.R
 import com.example.maincameleon.databinding.FragmentCameraBinding
 import java.io.File
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Locale
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class CameraFragment : Fragment() {
 
-    private lateinit var imageCapture: ImageCapture
-    private lateinit var cameraExecutor: ExecutorService
-
     private var _binding: FragmentCameraBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var imageCapture: ImageCapture
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         _binding = FragmentCameraBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+        val root: View = binding.root
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        // Initialize camera functionality
+        // Request camera permissions
         if (allPermissionsGranted()) {
             startCamera()
         } else {
             ActivityCompat.requestPermissions(
-                requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+                requireActivity(),
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
             )
         }
 
+        // Set up the listener for the capture button
         binding.cameraCaptureButton.setOnClickListener { takePhoto() }
 
+        // Set up the back button to navigate back to PhotoGalleryFragment
+        binding.backButton.setOnClickListener {
+            findNavController().navigateUp() // Navigate back to the previous fragment
+        }
+
+        outputDirectory = getOutputDirectory()
         cameraExecutor = Executors.newSingleThreadExecutor()
+
+        return root
     }
 
     private fun takePhoto() {
-        val imageCapture = this.imageCapture
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
 
-        // Create a folder for images if it doesn't exist
-        val storageDir = File(requireContext().getExternalFilesDir(null), "MyCameraImages")
-        if (!storageDir.exists()) {
-            storageDir.mkdirs() // Create the directory if it doesn't exist
-        }
-
-        // Create time-stamped output file within the new directory
+        // Create time-stamped output file to hold the image
         val photoFile = File(
-            storageDir,
-            "${SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis())}.jpg"
+            outputDirectory,
+            SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis()) + ".jpg"
         )
 
+        // Create output options object which contains file + metadata
         val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
 
+        // Set up image capture listener, which is triggered after photo has been taken
         imageCapture.takePicture(
-            outputOptions, ContextCompat.getMainExecutor(requireContext()),
+            outputOptions,
+            ContextCompat.getMainExecutor(requireContext()),
             object : ImageCapture.OnImageSavedCallback {
                 override fun onError(exc: ImageCaptureException) {
                     Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
                 }
 
                 override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                    val savedUri = Uri.fromFile(photoFile)
-                    Toast.makeText(context, "Photo captured: $savedUri", Toast.LENGTH_SHORT).show()
+                    val msg = "Photo capture succeeded: ${photoFile.absolutePath}"
+                    Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
                 }
             }
         )
     }
 
-
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
 
         cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
-            val preview = Preview.Builder().build().also {
-                it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
-            }
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
 
-            imageCapture = ImageCapture.Builder().build()
-
+            // Select back camera as a default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
             try {
+                // Unbind use cases before rebinding
                 cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(viewLifecycleOwner, cameraSelector, preview, imageCapture)
+
+                // Bind use cases to camera
+                imageCapture = ImageCapture.Builder().build()
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture
+                )
 
             } catch (exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
@@ -111,20 +133,18 @@ class CameraFragment : Fragment() {
         }, ContextCompat.getMainExecutor(requireContext()))
     }
 
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    private fun getOutputDirectory(): File {
+        val mediaDir = requireContext().externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else requireContext().filesDir
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String>, grantResults: IntArray
-    ) {
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera()
-            } else {
-                Toast.makeText(context, "Permissions not granted.", Toast.LENGTH_SHORT).show()
-            }
-        }
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            requireContext(), it
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/maincameleon/ui/photo_gallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/maincameleon/ui/photo_gallery/PhotoGalleryFragment.kt
@@ -1,22 +1,20 @@
-package com.example.maincameleon.ui.photo_gallery;
-
-
+package com.example.maincameleon.ui.photo_gallery
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.maincameleon.R
 import com.example.maincameleon.databinding.FragmentGalleryBinding
 
 class PhotoGalleryFragment : Fragment() {
 
-
     private var _binding: FragmentGalleryBinding? = null
-
-    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -34,6 +32,13 @@ class PhotoGalleryFragment : Fragment() {
         photoGalleryViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        // Find the button and set an OnClickListener
+        val button: Button = binding.button2 // Assuming button2 is your button ID
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_photoGalleryFragment_to_cameraFragment)
+        }
+
         return root
     }
 
