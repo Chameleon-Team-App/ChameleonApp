3233f622afa71bfc52555f688dd7566ef6b032b4
EthanEvirs <coloredcams@gmail.com>
2024-10-25 01:32:45 -0700

    removed navbar header

--- app/src/main/java/com/example/mainchameleon/MainActivity.kt
+++ app/src/main/java/com/example/mainchameleon/MainActivity.kt
@@ -5,7 +5,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.mainchameleon.databinding.ActivityMainBinding
 
@@ -29,7 +28,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_journal, R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_photo_gallery, R.id.navigation_calendar
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        //removed navbar header
         navView.setupWithNavController(navController)
 
         navView.setOnItemSelectedListener { item ->
