e134da7ea629713d5c3e059c8bf11ae3e8ec2b9c
ChristopherArias-Hash <christopher.ariass855@myci.csuci.edu>
2024-11-20 01:51:34 -0800

    back button in friends list works

--- app/src/main/java/com/example/mainchameleon/ui/userProfile/FriendsListFragment.kt
+++ app/src/main/java/com/example/mainchameleon/ui/userProfile/FriendsListFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.mainchameleon.R
 
 class FriendsListFragment : Fragment() {
@@ -14,6 +16,14 @@ class FriendsListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends_lists, container, false)
+        val view = inflater.inflate(R.layout.fragment_friends_lists, container, false)
+
+        // Set up back button to navigate to the profile
+        val backButton: ImageButton = view.findViewById(R.id.back_button)
+        backButton.setOnClickListener {
+            findNavController().navigate(R.id.navigation_profile)
+        }
+
+        return view
     }
 }
